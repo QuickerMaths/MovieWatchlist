1b2cdf604acae4c8c915453109c81247022539a3
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-06 12:52:23 +0200

    feat: implemented InMemoryMovieWatchlistRepository

--- MovieWatchlist/Repositories/InMemoryMovieWatchlistRepository.cs
+++ MovieWatchlist/Repositories/InMemoryMovieWatchlistRepository.cs
@@ -5,33 +5,80 @@ namespace MovieWatchlist.Repositories;
 
 public class InMemoryMovieWatchlistRepository : IWatchlistRepository<MovieWatchlistItem>
 {
-    public Task<IReadOnlyList<MovieWatchlistItem>?> GetByUserAsync(string userId, WatchStatus? status = null)
+    private readonly Dictionary<string, MovieWatchlistItem> _items = new();
+
+    public InMemoryMovieWatchlistRepository()
     {
-        throw new NotImplementedException();
+        var watchListItem = new MovieWatchlistItem
+        {
+            Id = "watchListItemId",
+            UserId = "userId",
+            MovieId = 1,
+            WatchStatus =  WatchStatus.WantToWatch,
+            Rating = 5,
+            Note = "Cool Movie",
+            AddedAt = DateTime.Now
+        };
+        
+        _items.Add(watchListItem.Id, watchListItem);
+    }
+    
+    public async Task<IEnumerable<MovieWatchlistItem>> GetByUserAsync(string userId, WatchStatus? status = null)
+    {
+        var items = _items.Values
+            .Where(item => item.UserId == userId && (status is null || item.WatchStatus == status))
+            .ToList();
+
+        return await Task.FromResult<IEnumerable<MovieWatchlistItem>>(items);
+    }
+    public async Task<MovieWatchlistItem?> GetByIdAsync(int id, string userId)
+    {
+        var watchListItem = _items.Values
+            .Where(item => item.UserId == userId && item.MovieId == id);
+        
+        return await Task.FromResult(watchListItem?.FirstOrDefault());
     }
 
-    public Task<MovieWatchlistItem?> GetByIdAsync(int id, string userId)
+    public async Task<bool> ExistsAsync(string userId, string movieId)
     {
-        throw new NotImplementedException();
+        var exists = _items.Values
+            .Any(item => item.UserId == userId && item.Id == movieId);
+        
+        return await Task.FromResult(exists);
     }
 
-    public Task<bool> ExistsAsync(string userId, string movieId)
+    public async Task<MovieWatchlistItem> AddAsync(MovieWatchlistItem model)
     {
-        throw new NotImplementedException();
+
+        if (await ExistsAsync(model.UserId, model.Id))
+        {
+            throw new InvalidOperationException("Watchlist item already exists");
+        }
+        
+        _items.Add(model.Id, model);
+
+        return model;
     }
 
-    public Task<MovieWatchlistItem> AddAsync(MovieWatchlistItem model)
+    public async Task<Task> UpdateAsync(MovieWatchlistItem model)
     {
-        throw new NotImplementedException();
+        if (await ExistsAsync("userId", "watchListItemId"))
+        {
+            throw new InvalidOperationException("Watchlist item does not exist");
+        }
+        
+        _items[model.Id] = model;
+
+        return Task.CompletedTask;
     }
 
-    public Task UpdateAsync(MovieWatchlistItem model)
+    public Task DeleteAsync(string movieId, string userId)
     {
-        throw new NotImplementedException();
-    }
+        if (_items.TryGetValue(movieId, out var item) && item.UserId == userId)
+        {
+            _items.Remove(movieId);
+        }
 
-    public Task DeleteAsync(string movieId)
-    {
-        throw new NotImplementedException();
+        return Task.CompletedTask;
     }
 }
