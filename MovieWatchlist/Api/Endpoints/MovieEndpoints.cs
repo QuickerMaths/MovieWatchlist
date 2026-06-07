155e649e6fea341e24811d315dd0cea54e4f50d3
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-07 15:02:18 +0200

    feat: added comments to endpoints for readbility

--- MovieWatchlist/Api/Endpoints/MovieEndpoints.cs
+++ MovieWatchlist/Api/Endpoints/MovieEndpoints.cs
@@ -8,7 +8,16 @@ public static class MovieEndpoints
     {
         var group = app.MapGroup("/movies");
 
+        /*
+         * Endpoint for Movie GET request "/movies/search?query="
+         * Search TMDB for movies by title
+         */
         group.MapGet("/search", ([FromQuery(Name = "search")] string query) => Results.Ok);
+        
+        /*
+         * Endpoint for Movie GET request "/movies/{tmdbId}"
+         * Get details for one movie from TMDB
+         */
         group.MapGet("/{id}", ([FromRoute(Name = "id")] string id) => Results.Ok());
 
         return app;
--- MovieWatchlist/Api/Endpoints/WatchlistEndpoints.cs
+++ MovieWatchlist/Api/Endpoints/WatchlistEndpoints.cs
@@ -9,10 +9,34 @@ public static class WatchlistEndpoints
     {
         var group = app.MapGroup("/watchlist").RequireAuthorization();
 
+        /*
+         * Endpoint for WatchlistItem GET request "/watchlist"
+         * Lists the current user's items
+         */
         group.MapGet(string.Empty,       () => Results.Ok());
+        
+        /*
+         * Endpoint for WatchlistItem GET request "/watchlist/{id}"
+         * Get a single watchlist item by Id
+         */
         group.MapGet("/{id}",       ([FromRoute(Name = "id")] string id) => Results.Ok());
+        
+        /*
+         * Endpoint for WatchlistItem POST request "/watchlist"
+         * Add a movie to the watchlist
+         */
         group.MapPost(string.Empty,       ([FromBody] AddWatchlistItemRequest request) => Results.Ok());
+        
+        /*
+         * Endpoint for WatchlistItem PUT request "/watchlist/{id}"
+         * Update status / rating / note
+         */
         group.MapPut("/{id}", ([FromRoute(Name = "id")] string id, [FromBody] UpdateWatchlistItemRequest request) => Results.Ok());
+        
+        /*
+         * Endpoint for WatchlistItem DELETE request "/watchlist/{id}"
+         * Remove an item
+         */
         group.MapDelete("/{id}", ([FromRoute(Name = "id")] string id) => Results.NoContent());
 
         return app;
