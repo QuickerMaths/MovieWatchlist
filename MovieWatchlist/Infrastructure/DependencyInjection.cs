18e8e2d2809b18605e5ccca21485cfe8b1d88234
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-07 09:40:50 +0200

    fix: fixed repository injection

--- MovieWatchlist/Infrastructure/DependencyInjection.cs
+++ MovieWatchlist/Infrastructure/DependencyInjection.cs
@@ -8,7 +8,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddSingleton<IMovieRepository<Movie>, IMovieRepository<Movie>>();
+        services.AddSingleton<IMovieRepository<Movie>, InMemoryMovieRepository>();
         services.AddSingleton<IWatchlistRepository<MovieWatchlistItem>, InMemoryMovieWatchlistRepository>();
 
         return services;
