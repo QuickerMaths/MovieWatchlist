edd2d9bc3a938d80525a883029fba538aec59a44
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-09 19:26:15 +0200

    refactor: change Movie properties to use init-only setters for immutability

--- MovieWatchlist/Domain/Entities/Movie.cs
+++ MovieWatchlist/Domain/Entities/Movie.cs
@@ -2,10 +2,10 @@ namespace MovieWatchlist.Domain.Entities;
 
 public class Movie
 {
-    public required string Id { get; set; }
-    public required int TmdbId { get; set; }
-    public required string Title  { get; set; }
-    public required string Overview  { get; set; }
-    public string? PosterPath { get; set; }
-    public required DateTime ReleaseDate { get; set; }
+    public string Id { get; set; }
+    public required int TmdbId { get; init; }
+    public required string Title  { get; init; }
+    public required string Overview  { get; init; }
+    public required string PosterPath { get; init; }
+    public required DateTime ReleaseDate { get; init; }
 }
