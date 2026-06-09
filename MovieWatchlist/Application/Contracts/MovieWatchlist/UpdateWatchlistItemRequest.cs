e30a7b46e95022874fecb757a977cbcfad5437e8
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-09 16:38:50 +0200

    refactor: rename Status to WatchStatus in UpdateWatchlistItemRequest

--- MovieWatchlist/Application/Contracts/MovieWatchlist/UpdateWatchlistItemRequest.cs
+++ MovieWatchlist/Application/Contracts/MovieWatchlist/UpdateWatchlistItemRequest.cs
@@ -2,4 +2,4 @@ using MovieWatchlist.Domain.Entities;
 
 namespace MovieWatchlist.Application.Contracts.MovieWatchlist;
 
-public record UpdateWatchlistItemRequest(WatchStatus? Status, int? Rating, string? Note);
+public record UpdateWatchlistItemRequest(WatchStatus? WatchStatus, int? Rating, string? Note);
