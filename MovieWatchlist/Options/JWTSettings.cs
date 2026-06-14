9936c2aa56d5d604c1fc7959e54ae8166e451103
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-14 12:08:27 +0200

    refactor: update JWTSettings properties to be non-nullable with default values

--- MovieWatchlist/Options/JWTSettings.cs
+++ MovieWatchlist/Options/JWTSettings.cs
@@ -2,8 +2,8 @@ namespace MovieWatchlist.Options;
 
 public class JWTSettings
 {
-    public string? Key { get; set; }
-    public string? Issuer { get; set; }
-    public string? Audience { get; set; }
-    public int? ExpiryMinutes { get; set; }
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int ExpiryMinutes { get; set; }
 }
