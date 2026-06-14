898d14f802a5e1f7a745a4f6d6b92d3ae1aa169e
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-06-14 11:29:28 +0200

    feat: add comments for authentication endpoints for login, logout, and registration

--- MovieWatchlist/Api/Endpoints/AuthEndpoints.cs
+++ MovieWatchlist/Api/Endpoints/AuthEndpoints.cs
@@ -9,8 +9,22 @@ public static class AuthEndpoints
     {
         var group = app.MapGroup("auth");
 
+        /*
+         * Endpoint for login POST request "/auth/login"
+         * Log in, receive a bearer token
+         */
         group.MapPost("login", ([FromBody]LoginRequest request ) => Results.Ok());
+        
+        /*
+         * Endpoint for logout POST request "/auth/logout"
+         * Logs out the user, revokes the token
+         */
         group.MapPost("logout", Results.NoContent);
+        
+        /*
+         * Endpoint for register POST request "/auth/register"
+         * Register a new user
+         */
         group.MapPost("register", ([FromBody] RegisterRequest request) => Results.Ok());
 
         return app;
