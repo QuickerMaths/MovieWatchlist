8d8b1092c603e3c5d11036e6b283b0f5d340270d
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-07-26 20:03:01 +0200

    feat: register WatchlistService and TMDB HttpClient in DI

--- MovieWatchlist/Infrastructure/DependencyInjection.cs
+++ MovieWatchlist/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MovieWatchlist.Application.Abstractions;
+using MovieWatchlist.Application.Services;
 using MovieWatchlist.Domain.Entities;
 using MovieWatchlist.Infrastructure.Identity;
 using MovieWatchlist.Infrastructure.Persistence;
@@ -23,6 +26,17 @@ public static class DependencyInjection
 
         services.AddSingleton<ITokenService, JwtTokenService>();
         services.AddSingleton<IRevokedTokenStore, InMemoryRevokedTokenStore>();
+
+        services.AddScoped<WatchlistService>();
+        services.AddHttpClient<ITmdbClient, TmdbClient>((sp, client) =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            client.BaseAddress = new Uri(config["Tmdb:BaseUrl"] ?? "https://api.themoviedb.org/3/");
+
+            var apiKey = config["Tmdb:ApiKey"];
+            if (!string.IsNullOrEmpty(apiKey))
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+        });
 
         return services;
     }
