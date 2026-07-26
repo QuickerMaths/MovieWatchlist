a3d53773a27e4a62aaecf0eaa46fdbddb77dc7e3
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-07-26 20:40:21 +0200

    feat: add Movie and WatchlistItem to the EF model

--- MovieWatchlist/Infrastructure/Persistence/AppDbContext.cs
+++ MovieWatchlist/Infrastructure/Persistence/AppDbContext.cs
@@ -1,8 +1,30 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MovieWatchlist.Domain.Entities;
 using MovieWatchlist.Infrastructure.Identity;
 
 namespace MovieWatchlist.Infrastructure.Persistence;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options)
-    : IdentityDbContext<ApplicationUser>(options);
+    : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+    public DbSet<MovieWatchlistItem> WatchlistItems => Set<MovieWatchlistItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Movie>(movie =>
+        {
+            movie.HasKey(m => m.Id);
+            movie.HasIndex(m => m.TmdbId).IsUnique();
+        });
+
+        builder.Entity<MovieWatchlistItem>(item =>
+        {
+            item.HasKey(i => i.Id);
+            item.HasIndex(i => new { i.UserId, i.MovieId }).IsUnique();
+        });
+    }
+}
